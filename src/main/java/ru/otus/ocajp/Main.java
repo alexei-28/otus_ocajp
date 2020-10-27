566674cf854d825eebc768a33206e66faa3fe64e
alexei.zhelev <alexei.jelev@qsystems.md>
2020-10-27 22:53:09 +0200

    init project

---
+++ src/main/java/ru/otus/ocajp/Main.java
@@ -0,0 +1,8 @@
+package ru.otus.ocajp;
+
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Hello, OTUS OCAJP!\nJDK: " + System.getProperty("java.version"));
+    }
+}

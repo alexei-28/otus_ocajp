e00657aed77ae84b61c85880b92f683d174c2e4b
alexei.zhelev <alexei.jelev@qsystems.md>
2020-11-02 21:34:55 +0200

    init homework01

---
+++ src/main/java/ru/otus/ocajp/homework01/Main.java
@@ -0,0 +1,14 @@
+package ru.otus.ocajp.homework01;
+
+/*-
+    Homework01's test:
+    https://docs.google.com/forms/d/e/1FAIpQLScJIB21Dp37XxsHVMuiebsrkphEHG6Kh26YIxyCu3XkyLtnGQ/viewform
+
+    Тест к занятию «Создание и запуск Java-приложений. Сравнение характеристик Java»
+ */
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Homework#01");
+    }
+}

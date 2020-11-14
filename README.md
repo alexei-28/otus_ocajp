1a95de2b20c2952b33cd2b7f4e856c07e94bc75b
alexei.zhelev <alexei.jelev@qsystems.md>
2020-11-14 18:05:34 +0200

    homework#02 finish

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # OTUS - OCAJP
 OTUS's course: Oracle Certified Java SE 8 Programmer I, 1Z0-808 (OCAJP)
+
+Важно!
+Продолжительность теста 90 мин.
+70 вопросов. В среднем 2 минуты на один вопрос.
---
+++ src/main/java/ru/otus/ocajp/ru/otus/ocajp/homework02/Main.java
@@ -0,0 +1,15 @@
+package ru.otus.ocajp.ru.otus.ocajp.homework02;
+
+import java.util.Date;
+
+/*-
+    Homework02 test:
+
+ */
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Homework#02");
+        Date date;
+    }
+}

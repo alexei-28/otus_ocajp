cd996bb889e47a50ca16674945be6e470f1436c6
alexei.zhelev <alexei.jelev@qsystems.md>
2020-11-22 20:03:24 +0200

    homework#04 in process

---
+++ src/main/java/ru/otus/ocajp/homework04/Main.java
@@ -0,0 +1,10 @@
+package ru.otus.ocajp.homework04;
+
+
+public class Main {
+
+    public static void main(String... arg) {
+        System.out.println("Homework04");
+        new Solution();
+    }
+}
---
+++ src/main/java/ru/otus/ocajp/homework04/Solution.java
@@ -0,0 +1,15 @@
+package ru.otus.ocajp.homework04;
+
+import java.util.Date;
+
+public class Solution {
+    private Date date;
+
+    public Solution() {
+        System.out.println(date);
+        final char a;
+        final char b = 8;
+        char c = a + b;
+    }
+
+}

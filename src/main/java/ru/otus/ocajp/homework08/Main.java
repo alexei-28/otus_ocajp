e2c63bf94ec37f17f0619b0b959e79b65822e7eb
alexei.zhelev <alexei.jelev@qsystems.md>
2020-12-26 13:05:56 +0200

    homework#08 finish

--- src/main/java/ru/otus/ocajp/homework08/Main.java
+++ src/main/java/ru/otus/ocajp/homework08/Main.java
@@ -1,4 +1,16 @@
 package ru.otus.ocajp.homework08;
 
 public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Homework#08");
+        int i = 0;
+        int k = i++;
+        System.out.println(i); // 0
+
+        int ii = 0;
+        ii++;
+        System.out.println(ii); // 1
+    }
+
 }

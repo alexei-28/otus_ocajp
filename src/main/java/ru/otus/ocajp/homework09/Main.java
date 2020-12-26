74cf60b5a6a9cd5a102aae6a6703f4c937f2966d
alexei.zhelev <alexei.jelev@qsystems.md>
2020-12-26 22:21:41 +0200

    update

---
+++ src/main/java/ru/otus/ocajp/homework09/Main.java
@@ -0,0 +1,11 @@
+package ru.otus.ocajp.homework09;
+
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Homework#09");
+        float test = 1_000f;
+        System.out.println("test = " + test);
+    }
+
+}

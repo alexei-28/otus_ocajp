66d2d37d0e4e68dd550809cbb4fd64731ba974fb
alexei.zhelev <alexei.jelev@qsystems.md>
2020-12-27 20:58:48 +0200

    homework#10 finish

--- src/main/java/ru/otus/ocajp/homework10/Main.java
+++ src/main/java/ru/otus/ocajp/homework10/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String... args) {
         System.out.println("Homework#10");
-        
-        System.out.println("" + 10 + 5 == 4 + 11 + ""); // false -> строка "105" слева и строка "15" справа
-        System.out.println("" + 10 + 5 == 50 + 55 + ""); // true -> строка "105" слева и строка "105" справа
+
+        System.out.println("" + 10 + 5 == 4 + 11 + "");// false -> строка "105" слева и строка "15" справа
+        System.out.println("" + 10 + 5 == 50 + 55 + "");// true -> строка "105" слева и строка "105" справа
     }
 
 }

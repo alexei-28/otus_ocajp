dec2df8c42d44bcb5d90b5bb6f16b34c35d7aa45
alexei.zhelev <a_subscriber@mail.ru>
2021-01-02 12:19:17 +0200

    update

--- src/main/java/ru/otus/ocajp/homework10/Main.java
+++ src/main/java/ru/otus/ocajp/homework10/Main.java
@@ -3,7 +3,7 @@ package ru.otus.ocajp.homework10;
 public class Main {
 
     public static void main(String... args) {
-        System.out.println("Homework#10");
+        System.out.println("Homework#10!\nJDK: " + System.getProperty("java.version"));
 
         System.out.println("" + 10 + 5 == 4 + 11 + "");// false -> строка "105" слева и строка "15" справа
         System.out.println("" + 10 + 5 == 50 + 55 + "");// true -> строка "105" слева и строка "105" справа

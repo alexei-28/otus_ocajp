1c31a756307a9d85efaf462da0a59a294767503b
alexei.zhelev <a_subscriber@mail.ru>
2021-01-02 16:05:54 +0200

    update finish 11

---
+++ src/main/java/ru/otus/ocajp/homework11/Main.java
@@ -0,0 +1,10 @@
+package ru.otus.ocajp.homework11;
+
+public class Main {
+    public static void main(String... args) {
+
+        System.out.println("Homework#11!\nJDK: " + System.getProperty("java.version"));
+
+    }
+
+}

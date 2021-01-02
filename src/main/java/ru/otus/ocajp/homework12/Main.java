804b62898b338a8cc017eaeeb2e16b0849a3a525
alexei.zhelev <a_subscriber@mail.ru>
2021-01-02 20:37:10 +0200

    homework#12 finish
    Author: alexei.zhelev <a_subscriber@mail.ru>
    Date:   Sat Jan 2 20:36:03 2021 +0200
    
        finish
    
    commit 1016f64585bb0d450e891e030db3e035bb91557c
    Author: alexei.zhelev <a_subscriber@mail.ru>
    Date:   Sat Jan 2 20:32:57 2021 +0200
    
        finish
    
    commit 4727c5e8c697fcc4ed341ef8c8174a1d453c06ab
    Author: alexei.zhelev <a_subscriber@mail.ru>
    Date:   Sat Jan 2 20:32:36 2021 +0200
    
        finish homework12

---
+++ src/main/java/ru/otus/ocajp/homework12/Main.java
@@ -0,0 +1,9 @@
+package ru.otus.ocajp.homework12;
+
+public class Main {
+    public static void main(String... args) {
+
+        System.out.println("Homework#12!\nJDK: " + System.getProperty("java.version"));
+
+    }
+}

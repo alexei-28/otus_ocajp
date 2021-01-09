809d11fb384f2e4c3eef66b659ced88518761b54
Alexei <a_subscriber@mail.ru>
2021-01-09 13:29:10 +0200

    homework#14 finish
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 9 13:28:33 2021 +0200
    
        c
    
    commit c200e488ef4ab7031c481cdc26a78e76deaec210
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 9 12:59:17 2021 +0200
    
        w
    
    commit 17d305dfe64c4dabb5e376bbe49dfd586f664d0f
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 9 12:35:27 2021 +0200
    
        c
    
    commit b7e05d1062391814f0ddbfa0b502498bb88c3794
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Fri Jan 8 14:21:53 2021 +0200
    
        w
    
    commit fc992a711ec528bc2b50b2bd7063d1403f238288
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Fri Jan 8 13:59:06 2021 +0200
    
        w
    
    commit eb5f73900bc2d5e96d08bb1db35f2b825fff8f98
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Fri Jan 8 13:57:19 2021 +0200
    
        w
    
    commit 19a65cb27295065d7c2706cee79e1776d5f5a252
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Fri Jan 8 12:52:12 2021 +0200
    
        w
    
    commit 2821b0338c1908bb45c1ad222c21efccbeb1222a
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Fri Jan 8 12:49:35 2021 +0200
    
        w
    
    commit f07572c3bd66ad0ee28e929192b71b1506ea9151
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Fri Jan 8 12:47:37 2021 +0200
    
        w
    
    commit ab50dc0f42e1df606e8567a6efe71fec89b7c838
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Fri Jan 8 12:33:47 2021 +0200
    
        c

--- src/main/java/ru/otus/ocajp/homework11/Main.java
+++ src/main/java/ru/otus/ocajp/homework11/Main.java
@@ -1,10 +1,9 @@
 package ru.otus.ocajp.homework11;
 
+// Логические операторы. Порядок выполнения
 public class Main {
     public static void main(String... args) {
-
         System.out.println("Homework#11!\nJDK: " + System.getProperty("java.version"));
-
     }
 
 }
---
+++ src/main/java/ru/otus/ocajp/homework13/Main.java
@@ -0,0 +1,9 @@
+package ru.otus.ocajp.homework13;
+
+// Применение оператора switch
+public class Main {
+    public static void main(String... args) {
+        System.out.println("Homework#13!\nJDK: " + System.getProperty("java.version"));
+
+    }
+}

ffb87c68571171eeab66afda99e677d4d7379933
Alexei <a_subscriber@mail.ru>
2021-01-16 18:17:09 +0200

    homework#17 finish
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 16 12:18:32 2021 +0200
    
        c
    
    commit f6e8b2a51656559da486181b3aa7c3b29199efd0
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 16 12:16:05 2021 +0200
    
        c
    
    commit bb38f23621d7f17a0f1ca90a23a4adb99d9f5586
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 16 12:04:00 2021 +0200
    
        c
    
    commit b36f09607680bd398030585c333575c9e9991ca3
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 16 11:18:08 2021 +0200
    
        update
    
    commit 81b3f9d0d1e2f6f87f9fc4eea1f33d9724126cb2
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 16 10:56:01 2021 +0200
    
        update

---
+++ src/main/java/ru/otus/ocajp/homework17/Main.java
@@ -0,0 +1,23 @@
+package ru.otus.ocajp.homework17;
+
+public class Main {
+    public static void main(String... args) {
+        System.out.println("Homework#16!\nJDK: " + System.getProperty("java.version"));
+
+        // Методы класса String создают новые объекты только если были изменения.
+        String str = "Hello";
+        System.out.println(str == str.replace("Z", "z")); // true
+        System.out.println(str == str.trim());// true
+        System.out.println(str == str.substring(0));// true
+        // 3 раза "true" так как не было изменений, а значит новый String объект НЕ создается.
+
+        String[] str2 = new String[2]; // array with size 2 and all items are null
+
+        // Java считает char целочисленным типом,
+        // представляющим значения от 0 до 65535 включительно → стало быть, комперра нет
+        char c = 50006; // OK
+        System.out.println(c);
+
+        // char c2 = 66535; // compile error
+    }
+}

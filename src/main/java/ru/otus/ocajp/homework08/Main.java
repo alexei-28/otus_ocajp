01fa1cb89b49f8e90fa4f408ed3827d897c02d61
Alexei <a_subscriber@mail.ru>
2021-01-24 11:56:51 +0200

    homework#19 finish
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sun Jan 24 11:56:33 2021 +0200
    
        c
    
    commit 6e737795ab7029b78c77b3ce0838cf863cf8770e
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sun Jan 24 11:24:04 2021 +0200
    
        c
    
    commit 3039d31bc1ec54bb5435348011b55cb120f6f575
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sun Jan 24 11:11:42 2021 +0200
    
        c
    
    commit 0a269a919534fd5d0b36d48e5952bd03496e6703
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sun Jan 24 10:56:02 2021 +0200
    
        c
    
    commit 79d146c44c34f1020c31a68712696f2bcdd674f8
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sun Jan 24 10:55:04 2021 +0200
    
        c
    
    commit 8058c1adc866fc4a87ad4721e8f6921995152637
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 23 16:38:10 2021 +0200
    
        c
    
    commit 07a07a06d199eee78f5c896aefd295385d676dfc
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 23 13:01:30 2021 +0200
    
        c
    
    commit 3bf274cd3f62e5f85230d17a88eb1fcc9e4f0865
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 23 12:55:58 2021 +0200
    
        c
    
    commit d544d3bbaaea4e6c8aa8ea5e98df131c92724331
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 23 12:46:00 2021 +0200
    
        w
    
    commit d63999de952927c70aa6b2a66dfe08086d83c9cc
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 23 12:29:24 2021 +0200
    
        c
    
    commit 8c4d27bb9ed096a70baa5abcea5a5f513636b5c0
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 23 12:08:09 2021 +0200
    
        c
    
    commit bfc5bdd12d6bbd557a1c157349743e6be74b4b67
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Jan 23 12:01:50 2021 +0200
    
        c
    
    commit d7dfb97e84e402e42a886b23e9494b4d654b5a22
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 20 20:04:28 2021 +0200
    
        w
    
    commit b9e332d6de9c451066b0c12dafcd9b311f3986bb
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 20 20:02:31 2021 +0200
    
        w
    
    commit 2f6dd9152e1867b71d53f240b22817549853f833
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 20 19:56:52 2021 +0200
    
        w
    
    commit cfcf1b20c5492e005a86cffc3e079d9cfe9b79d2
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 20 19:15:06 2021 +0200
    
        c
    
    commit 32bab64dd9c325aa0b40f85a955d12971aa779ab
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 20 19:06:10 2021 +0200
    
        c
    
    commit 4591e4bb1255a14bb126f8a4036b7f3fc06345a9
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 20 19:02:14 2021 +0200
    
        w

--- src/main/java/ru/otus/ocajp/homework08/Main.java
+++ src/main/java/ru/otus/ocajp/homework08/Main.java
@@ -5,12 +5,16 @@ public class Main {
     public static void main(String... args) {
         System.out.println("Homework#08");
         int i = 0;
-        int k = i++;
-        System.out.println(i); // 0
+        int i2 = i++; // i never used
+        System.out.println(i2); // 0
 
-        int ii = 0;
-        ii++;
-        System.out.println(ii); // 1
+        int i3 = 0;
+        i3 = i3 + 1;
+        System.out.println(i3); // 1
+
+        int i4 = 0;
+        i4++;
+        System.out.println(i4); // 1
     }
 
 }
--- src/main/java/ru/otus/ocajp/homework10/Main.java
+++ src/main/java/ru/otus/ocajp/homework10/Main.java
@@ -7,6 +7,29 @@ public class Main {
 
         System.out.println("" + 10 + 5 == 4 + 11 + "");// false -> строка "105" слева и строка "15" справа
         System.out.println("" + 10 + 5 == 50 + 55 + "");// true -> строка "105" слева и строка "105" справа
+
+        // Поменять местами 2 числа без использования 3-й переменной
+        int a = 2, b = 4;
+        System.out.println("before swap: a = " + a + ", b = " + b);
+        a = a + b; // a = 2 + 4 = 6
+        b = a - b; // b = 6 - 4 = 2
+        a = a - b; // a = 6 - 2 = 4
+        System.out.println("after swap:  a = " + a + ", b = " + b);
+
+        short h = 40;
+        // h = (short) h + (short) 2; // compile error, promotion to int
+        h = (short) ((short) h + (short) 2); // ok
+        System.out.println("h = " + h); // 42
+
+        short s = 10;
+        //Integer i = s; // compile error
+        int si = s;
+        Integer i = (int) s;
+        System.out.println("i = " + i +", si = " + si);
+
+        int test = 1;
+        test += test++ - --test;
+        System.out.println("test = " + test);
     }
 
 }

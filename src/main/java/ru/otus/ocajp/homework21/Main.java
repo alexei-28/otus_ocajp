483dc80ad1bdca58919dd36907cf2c0f8a57f6fc
Alexei <a_subscriber@mail.ru>
2021-01-28 21:39:41 +0200

    homework21 finish
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Thu Jan 28 21:39:02 2021 +0200
    
        c
    
    commit f45485c4bce895a4f0f3b3a403acc238449c5be4
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 21:28:10 2021 +0200
    
        c
    
    commit 7a26ede85c13c253b062004aa859502bf434ed40
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 21:21:57 2021 +0200
    
        c
    
    commit 5c8cb1004a304858d942fcb84537879814e7670e
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 21:05:07 2021 +0200
    
        c
    
    commit 584a631df54d3429579296d5e6f90319a0388cd7
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 20:50:54 2021 +0200
    
        c
    
    commit 6477a9475994333700ded473d464c941de06362e
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 20:37:10 2021 +0200
    
        c
    
    commit f330ca319f85339d10e7d414d1e5f74f17eb4fb7
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 20:35:41 2021 +0200
    
        c
    
    commit 482e26d79b9a565faf35dd9c1bd9cf1921525038
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 19:38:58 2021 +0200
    
        c
    
    commit 70a24b85d35d4a06c5d97779d85cc9dad8d6f0a3
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 19:31:26 2021 +0200
    
        c
    
    commit fb2a9eb894988570aabe26ea61134972bad7fc9a
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 19:25:29 2021 +0200
    
        c
    
    commit 1c03df218b530453e3de77fd65200640f0fe897e
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 19:22:41 2021 +0200
    
        c
    
    commit 6d3485fabec2813160817d89a11f453ce77e0b4e
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 19:19:32 2021 +0200
    
        c
    
    commit b72006c5aac008a80777932c5bb111964c500abf
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 19:17:00 2021 +0200
    
        c
    
    commit 186e3379fdcafb3d80159762ab373892f5ecd9d2
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 19:14:33 2021 +0200
    
        c
    
    commit b221dec032ab7f7b55539a9f9ae4938685346af5
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 19:07:22 2021 +0200
    
        c
    
    commit bd9d7e1c8422279c69320920c97fcd1fa1213636
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 18:55:46 2021 +0200
    
        c
    
    commit 1e5dd41fe360edfb966f7e6b6c7ae5dacc59aaf2
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Jan 27 18:51:06 2021 +0200
    
        c
    
    commit 220704597e26bb8918d2675931f4cfbe94256782
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Mon Jan 25 22:33:19 2021 +0200
    
        c
    
    commit fe0876ff4705cbcae2eb63a82d1bad83efa4c3b0
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Mon Jan 25 22:23:59 2021 +0200
    
        c
    
    commit 3adc59c5f69b571a1ba94368dce93a5b4d91b07b
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Mon Jan 25 22:15:18 2021 +0200
    
        c
    
    commit 607a2124a301465d0fb3a93f6203f437aae21198
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Mon Jan 25 21:20:40 2021 +0200
    
        c
    
    commit 1676801a4ac29de81feed97ca21ee60ee05a4967
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Mon Jan 25 21:20:16 2021 +0200
    
        c
    
    commit f93e4776c14975da9e95a46a19c1aa0b3634f6c5
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Mon Jan 25 21:20:11 2021 +0200
    
        c

---
+++ src/main/java/ru/otus/ocajp/homework21/Main.java
@@ -0,0 +1,165 @@
+package ru.otus.ocajp.homework21;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Date;
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Homework#21.\nJDK: " + System.getProperty("java.version"));
+
+        LocalDate ld = LocalDate.of(2015, Month.APRIL, 12);
+        ld.plusDays(10);
+        System.out.println("ld = " + ld); // 2015-04-12, LocalDateXXX is immutable classes
+
+        LocalDate ld2 = LocalDate.of(2015, Month.APRIL, 12);
+        ld2 = ld.plusMonths(2).plusDays(10);
+        System.out.println("ld2 = " + ld2); // 2015-06-22
+
+        //LocalDate ld = LocalDate.of(2015, Month.APRIL, 32); // throw DateTimeException
+
+        LocalTime lt = LocalTime.NOON.plusMinutes(120);
+        System.out.println("lt = " + lt); // 14:00
+
+        LocalDate ld3 = LocalDate.of(2020, 1, 30);
+        //ld3.plusMinutes(120); // compile error
+
+        Period period = Period.of(1, 0, 7); // every year and 7 days
+        System.out.println("period = " + period); // "P1Y7D"
+
+        LocalDate ld4 = LocalDate.of(1, 1, 1).plus(Period.of(2009, 7, 12));
+        System.out.println("ld4 = " + ld4); // 2010-08-13
+
+        LocalDate ld5 = LocalDate.of(0, 10, 21);
+        System.out.println("ld5 = " + ld5); // 0000-10-21
+
+        Period period1 = Period.of(2, 4, 8);
+        System.out.println(period1); // P2Y4M8D
+
+        Period period123Y = period1.withYears(123);
+        System.out.println("period123Y = " + period123Y); // P123Y4M8D
+
+        Period period2 = period1.withMonths(7);
+        // isNegative() - Checks if any of the three units(y,M,d) of this period are negative.
+        System.out.println("period2 = " + period2 + ", isNegative = " + period2.isNegative()); // P2Y7M8D, isNegative = false
+
+        Period period3 = period1.withMonths(-7);
+        // isNegative() - Checks if any of the three units(y,M,d) of this period are negative.
+        System.out.println("period3 = " + period3 + ", isNegative = " + period3.isNegative()); // P2Y-7M8D, isNegative = true
+
+
+        Period period15D = period1.withDays(15);
+        System.out.println("period15D = " + period15D); // P2Y4M15D
+
+        Period periodPlusDays15 = period1.plusDays(15);
+        System.out.println("periodPlusDays15 = " + periodPlusDays15); // P2Y4M23D
+
+        Period periodParse = Period.parse("P2Y4M23D");
+        System.out.println("periodParse = " + periodParse); // P2Y4M23D
+
+        Period periodTest = Period.ofDays(1);
+        System.out.println("periodTest = " + periodTest); // P1D
+
+        Period periodTest2 = Period.ofDays(1).ofMonths(34); // Period does not allow chaining. Only the last Period method called counts
+        System.out.println("periodTest2 = " + periodTest2); // P34M
+
+        DateTimeFormatter dtf = DateTimeFormatter.ISO_DATE;
+        LocalDate localDate = LocalDate.of(2021, 10, 30);
+        String dateStr = localDate.format(dtf);
+        System.out.println("dateStr = " + dateStr); // 2021-10-30
+
+        DateTimeFormatter dtf2 = DateTimeFormatter.ISO_LOCAL_DATE;
+        LocalDate localDate2 = LocalDate.of(2021, 10, 30);
+        String dateStr2 = localDate2.format(dtf2);
+        System.out.println("dateStr2 = " + dateStr2); // 2021-10-30
+
+        DateTimeFormatter dtf3 = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+        LocalDate localDate3 = LocalDate.of(2021, 10, 30);
+        //String dateStr3 = localDate3.format(dtf3); // error UnsupportedTemporalTypeException
+        //System.out.println("dateStr3 = " + dateStr3); // 2021-10-30
+
+        DateTimeFormatter dtf4 = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+        LocalDateTime localDateTime = LocalDateTime.of(2021, 10, 30, 13, 56);
+        String dateStr4 = localDateTime.format(dtf4);
+        System.out.println("dateStr4 = " + dateStr4); // 2021-10-30T13:56:00
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
+        String dateStr5 = localDateTime.format(dateTimeFormatter);
+        System.out.println("dateStr5 = " + dateStr5); // 10/30/21 1:56 PM
+
+        DateTimeFormatter dateTimeFormatter2 = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
+        LocalTime localTime = LocalTime.of(10, 56);
+        //String dateStr6 = localTime.format(dateTimeFormatter2); // // error UnsupportedTemporalTypeException
+        //System.out.println("dateStr6 = " + dateStr6); // 1:56 PM
+
+        DateTimeFormatter dateTimeFormatter3 = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+        LocalTime localTime2 = LocalTime.of(20, 56);
+        String dateStr7 = localTime2.format(dateTimeFormatter3);
+        System.out.println("dateStr7 = " + dateStr7); // 8:56 PM
+
+        DateTimeFormatter dateTimeFormatter4 = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+        LocalDate localDate8 = LocalDate.of(2021, 1, 27);
+        String dateStr8 = localDate8.format(dateTimeFormatter4);
+        System.out.println("dateStr8 = " + dateStr8); // 1/27/21
+
+        DateTimeFormatter dateTimeFormatter5 = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+        LocalDateTime localDate5 = LocalDateTime.of(2021, 1, 27, 12, 42);
+        String dateTimeStr6 = localDate5.format(dateTimeFormatter5);
+        System.out.println("dateTimeStr6 = " + dateTimeStr6); // 12:42 PM
+
+        DateTimeFormatter dateTimeFormatter9 = DateTimeFormatter.ofPattern("dd.MM.yyy HH:mm");
+        LocalDateTime ldt9 = LocalDateTime.parse("27.01.2021 14:45", dateTimeFormatter9);
+        System.out.println("ldt9 = " + ldt9); // 2021-01-27T14:45
+
+        //LocalDateTime ldt10 = LocalDateTime.parse("27.01.202", dateTimeFormatter9);  // throw DateTimeParseException
+        //System.out.println("ldt10 = " + ldt10);
+
+        // By default use DateTimeFormatter.ISO_LOCAL_DATE (yyyy-MM-dd)
+        //LocalDate ld11 = LocalDate.parse("27.01.202T10:12"); // throw DateTimeParseException
+        //System.out.println("ld11 = " + ld11);
+
+        // By default use DateTimeFormatter.ISO_LOCAL_TIME (HH:mm:ss)
+        //LocalTime lt12 = LocalTime.parse("11:2");  // throw DateTimeParseException (2 digits for hours and minutes)
+        //System.out.println("lt12 = " + lt12); //
+
+        DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern("MM/dd/yyyy 'at' HH:mm:ss");
+
+        LocalDate localDate1 = LocalDate.parse("01/29/2015 at 00:15:30", dateTimeFormatter1);
+        System.out.println("localDate1 = " + localDate1 + " -> isLeapYear = " + localDate1.isLeapYear());
+        System.out.println("localDate1 = " + localDate1); // 2015-01-29
+
+        LocalTime localTime1 = LocalTime.parse("01/29/2015 at 00:15:30", dateTimeFormatter1);
+        System.out.println("localTime1 = " + localTime1); // 00:15:30
+
+        LocalDateTime ldt13 = LocalDateTime.of(2021, Month.APRIL, 22, 14, 34);
+        //System.out.println("isLeapYear = " + ldt13.isLeapYear()); // compile error
+        LocalDateTime ldt14 = LocalDateTime.of(2021, Month.APRIL, 22, 14, 34);
+        System.out.println("ldt13 == ldt14: " + ldt13.equals(ldt14)); // true
+
+        LocalTime lt15 = LocalTime.MAX;
+        System.out.println("lt15 = " + lt15); // 23:59:59.999999999
+
+        LocalDate ld16 = LocalDate.MAX;
+        System.out.println("ld16 =  " + ld16); // +999999999-12-31
+
+        LocalDate ld17 = LocalDate.of(2015, 3, 22);
+        //ld17 = ld17.withYear(13); // Returns a copy of this LocalDate with the year altered -> 0013-03-22
+        ld17.withMonth(1); // LocalDate is IMMUTABLE. As result date not change
+        // LocalDate.getMonth() method returns a Month enum constant
+        System.out.println("ld17 = " + ld17.getYear() + "|" + ld17.getMonth() + "|" + ld17.getDayOfMonth()); // 2015|MARCH|22
+
+        LocalDate ld18 = LocalDate.of(2021, 1, 12);
+        LocalDate ld19 = LocalDate.of(2021, 2, 12);
+        System.out.println(ld18.isAfter(ld19)); // false
+        System.out.println(ld18.isBefore(ld19)); // true
+
+        // between - Obtains a Period consisting of the number of years, months, and days between two dates. Return Period
+        System.out.println(Period.between(ld18, ld19).isNegative()); // false
+
+        // until - Calculates the period between this date and another date as a Period. Return Period
+        System.out.println(ld18.until(ld19).isNegative()); // false
+
+
+    }
+}

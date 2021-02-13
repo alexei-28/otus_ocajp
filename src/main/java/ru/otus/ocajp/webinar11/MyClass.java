053e898d6c323db70d1a570f3bea2c6e6260d293
Alexei <a_subscriber@mail.ru>
2021-02-13 15:02:33 +0200

    homework11: repeat
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Sat Feb 13 15:02:18 2021 +0200
    
        c
    
    commit 28722e40735850700cf5b4b00b7e6b9ebda12cd3
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Fri Feb 12 23:01:34 2021 +0200
    
        c

---
+++ src/main/java/ru/otus/ocajp/webinar11/MyClass.java
@@ -0,0 +1,18 @@
+package ru.otus.ocajp.webinar11;
+
+//private class MyClass { // compile error. modifier private not allowed here
+//protected class MyClass { // compile error. modifier protected not allowed here
+public class MyClass {
+
+    public MyClass() {
+
+    }
+
+    private class MyInnerClass {
+
+    }
+
+    protected class MyInnerClass2 {
+
+    }
+}

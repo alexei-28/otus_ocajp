9a9534558d7205ec2ffee81896612788feeb0a4b
Alexei <a_subscriber@mail.ru>
2021-02-16 16:51:27 +0200

    webinar23 repeat
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Tue Feb 16 16:51:08 2021 +0200
    
        c
    
    commit 066924e5f494a726168da87a45241313693de612
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Tue Feb 16 14:43:01 2021 +0200
    
        c

---
+++ src/main/java/ru/otus/ocajp/webinar22/MyFunctionInterface.java
@@ -0,0 +1,20 @@
+package ru.otus.ocajp.webinar22;
+
+// This function interface has 2 abstract methods.
+// It's work because second abstract method "equals()" is override from Object class.
+@FunctionalInterface
+public interface MyFunctionInterface {
+
+    default void myDefaultMethod() {
+        System.out.println("myDefaultMethod");
+    }
+
+    static void testMethod() {
+        System.out.println("static testMethod");
+    }
+
+    public boolean myMethod();
+
+    @Override
+    public boolean equals(Object obj);
+}

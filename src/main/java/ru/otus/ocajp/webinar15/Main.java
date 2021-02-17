2af5d97277b928eb2a6bd31ff5db41b8bd12ddcc
Alexei <a_subscriber@mail.ru>
2021-02-17 13:10:50 +0200

    homework15: finish repeat
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Feb 17 13:10:30 2021 +0200
    
        c
    
    commit 47d2c815bf9c666951fe3fe98df1cca9842e96ea
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Feb 17 12:31:22 2021 +0200
    
        c
    
    commit 47517876037287e73b5813bf61dfb55928de90d8
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Feb 17 12:01:13 2021 +0200
    
        c
    
    commit ebea25790a96626b4e0dbda62ba219123f9c2b0b
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Feb 17 11:51:42 2021 +0200
    
        c
    
    commit c88c8ae7e588117d608fbd7e24729401d2c96fad
    Author: Alexei <a_subscriber@mail.ru>
    Date:   Wed Feb 17 11:49:23 2021 +0200
    
        c

--- src/main/java/ru/otus/ocajp/webinar15/Main.java
+++ src/main/java/ru/otus/ocajp/webinar15/Main.java
@@ -5,5 +5,54 @@ public class Main {
 
     public static void main(String args[]) {
         System.out.println("Webinar#15!\nJDK: " + System.getProperty("java.version"));
+        System.out.println("Test1");
+        label:
+        if (true) {
+            label2:
+            for (int i = 0; i < 3; i++) {
+                System.out.println("i = " + i);
+                //continue label; // compile error, not a loop label
+                continue label2; // OK, but warning: "continue" is unnecessary as the last statement in a loop
+                //continue; // OK, but warning: "continue" is unnecessary as the last statement in a loop
+            }
+        }
+        System.out.println("Test2");
+        label3:
+        for (int i = 0; i < 2; i++) {
+            label4:
+            for (int j = 0; j < 2; j++) {
+                System.out.println("i = " + i + ", j = " + j);
+                continue label3;
+            }
+        }
+
+        System.out.println("Test3");
+        BUNNY:
+        for (int i = 0; i < 4; i++) {
+            RABBIT:
+            for (int j = 0; j < 4; j++) { // always true
+                System.out.println("i = " + i + ", j = " + j);
+                continue BUNNY;
+            }
+            System.out.println("after RABBIT(inner)"); // never print
+        }
+        System.out.println("after BUNNY(outer)");
+
+        System.out.println("Test4");
+        while (true) {
+            System.out.println("while_and_break");
+            break;
+        }
+
+        System.out.println("Test5");
+        int testIn = 5;
+        do {
+            System.out.println("do_while_break, testIn = " + testIn);
+            testIn--;
+            if (testIn == 3) {
+                break;
+            }
+            continue; // "continue" is unnecessary as the last statement in a loop
+        } while (testIn != 0);
     }
 }

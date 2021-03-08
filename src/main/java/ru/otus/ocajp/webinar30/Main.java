c183ff5b3e856ffc1429c13f45fc9b0910c02f4c
Alexei <a_subscriber@mail.ru>
2021-03-08 17:28:00 +0200

    update

---
+++ src/main/java/ru/otus/ocajp/webinar30/Main.java
@@ -0,0 +1,7 @@
+package ru.otus.ocajp.webinar30;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Webinar#30.\nJDK: " + System.getProperty("java.version"));
+    }
+}

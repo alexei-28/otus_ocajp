d5810071a81144554bcd6db8506790df1bb79727
Alexei Zhelev <a_subscriber@mail.ru>
2021-04-03 21:02:31 +0300

    add new module (examsnet)

---
+++ examsnet/src/main/java/ru/otus/ocajp/examsnet/Main.java
@@ -0,0 +1,8 @@
+package ru.otus.ocajp.examsnet;
+
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println("Hello, Examsnet!\nJDK: " + System.getProperty("java.version"));
+    }
+}

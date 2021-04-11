14bc88654afde1db327aa3b48bc8993397f30d76
Alexei Zhelev <a_subscriber@mail.ru>
2021-04-11 11:52:51 +0300

    homework07-repeat, pass
    commit 632e5613a4c3370c8fb1dc6ed3c444a5ee20fada
    Author: Alexei Zhelev <a_subscriber@mail.ru>
    Date:   Sun Apr 11 11:52:31 2021 +0300
    
        update homework07 repeate, pass
    
    commit 4098958a605d8eca60edac63eb5270f8cde59960
    Author: Alexei Zhelev <a_subscriber@mail.ru>
    Date:   Sun Apr 11 11:52:13 2021 +0300
    
        homework07-repeate - pass

--- src/main/java/ru/otus/ocajp/webinar07/Main.java
+++ src/main/java/ru/otus/ocajp/webinar07/Main.java
@@ -5,6 +5,10 @@ public class Main {
 
     public static void main(String... args) {
         System.out.println("Webinar#07.\nJDK: " + System.getProperty("java.version"));
+
+        boolean test = true;
+        // FALSE не является валидным ключевым словом для boolean-литерала
+        //boolean test2 = FALSE; // compile error
     }
 
 }

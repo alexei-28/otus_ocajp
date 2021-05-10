bb0135830e49027cb297b134d2f6bab0a991c492
Alexei Zhelev <a_subscriber@mail.ru>
2021-05-10 14:58:20 +0300

    update whizlabs tests
    commit dd2a93333cadee7d5852b0c7937e375837ce4bcd
    Author: Alexei Zhelev <a_subscriber@mail.ru>
    Date:   Mon May 10 14:58:04 2021 +0300
    
        update

---
+++ whizlabs/src/main/java/com/whizlabs/sectiontests/quiz13/Main.java
@@ -0,0 +1,12 @@
+package com.whizlabs.sectiontests.quiz13;
+
+/*-
+    Whizlabs: Practice Test -> Section Test -> 13: Working with Methods and Encapsulation
+    https://www.whizlabs.com/learn/course/ocajp-8-online-course/quiz/13843
+
+ */
+public class Main {
+    public static void main(String... args) {
+        System.out.println("Working with Methods and Encapsulation.\nJDK: " + System.getProperty("java.version"));
+    }
+}

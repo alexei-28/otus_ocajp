b332b812444b1ea4900bfd13a971110d407d5f9d
Alexei Zhelev <a_subscriber@mail.ru>
2021-05-22 13:31:49 +0300

    update

--- whizlabs/src/main/java/com/whizlabs/sectiontests/quiz13/Main.java
+++ whizlabs/src/main/java/com/whizlabs/sectiontests/quiz13/Main.java
@@ -7,5 +7,11 @@ package com.whizlabs.sectiontests.quiz13;
 public class Main {
     public static void main(String... args) {
         System.out.println("13 Quiz: Using Loop Constructs.\nJDK: " + System.getProperty("java.version"));
+
+        int i = 10;
+        while(true) {
+            i = 20;
+            System.out.println("i = " + i);
+        }
     }
 }

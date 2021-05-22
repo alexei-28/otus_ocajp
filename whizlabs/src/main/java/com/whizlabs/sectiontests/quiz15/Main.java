aa3dff9dbd595a276a82163557d69fe3ee1590dd
Alexei Zhelev <a_subscriber@mail.ru>
2021-05-22 22:03:35 +0300

    w

---
+++ whizlabs/src/main/java/com/whizlabs/sectiontests/quiz15/Main.java
@@ -0,0 +1,12 @@
+package com.whizlabs.sectiontests.quiz15;
+
+/*-
+    Whizlabs: Practice Test -> Section Test -> 15 Quiz: Working with Inheritance
+    https://www.whizlabs.com/learn/course/ocajp-8-online-course/quiz/13843
+
+ */
+public class Main {
+    public static void main(String... args) {
+        System.out.println("15 Quiz: Working with Inheritance.\nJDK: " + System.getProperty("java.version"));
+    }
+}

7295eb62e739045efbd013d01fa9482673931ea5
Alexei <a_subscriber@mail.ru>
2022-11-12 11:50:24 +0200

    Optimized import

--- src/main/java/ru/otus/ocajp/webinar02/Main.java
+++ src/main/java/ru/otus/ocajp/webinar02/Main.java
@@ -1,7 +1,5 @@
 package ru.otus.ocajp.webinar02;
 
-import java.util.Date;
-
 /*-
   Определение структуры Java-класса. Импорт Java-пакетов.
   Presentation: https://cdn.otus.ru/media/private/f3/04/03___OCA_Java_Basics-1801-f30405.pdf?hash=AI0uRlyJWkcwc-iKVJZoXQ&expires=1605657292

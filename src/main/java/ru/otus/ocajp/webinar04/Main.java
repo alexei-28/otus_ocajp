c4d86b03ae5fe1ec9ce09979798cae58a1b2dbd5
Alexei <a_subscriber@mail.ru>
2022-11-20 13:40:05 +0200

    update tests

--- src/main/java/ru/otus/ocajp/webinar04/Main.java
+++ src/main/java/ru/otus/ocajp/webinar04/Main.java
@@ -1,6 +1,10 @@
 package ru.otus.ocajp.webinar04;
 
-// Объявление и инициализация переменных (включая приведение примитивных типов данных)
+/*-
+    Объявление и инициализация переменных (включая приведение примитивных типов данных)
+    Test: https://docs.google.com/forms/d/e/1FAIpQLScycljT6BOJservmUA13ly2aclZLyNVob2YTMQJmo-vm2npAA/viewform
+    Answer: https://docs.google.com/forms/d/e/1FAIpQLScycljT6BOJservmUA13ly2aclZLyNVob2YTMQJmo-vm2npAA/viewscore?viewscore=AE0zAgA8KWO1Qx5KlLVCP9OnmOencnBop8YaZuDjiQrVsbZ9XiVArsOp_YL9dcqr3xYS1UY
+ */
 public class Main {
 
     public static void main(String... arg) {

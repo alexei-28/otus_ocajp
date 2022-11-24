8d6da158f344acb51b5b6a346c137ae47b37be2d
Alexei <a_subscriber@mail.ru>
2022-11-24 21:02:03 +0200

    pool of numbers

--- src/main/java/ru/otus/ocajp/webinar07/Main.java
+++ src/main/java/ru/otus/ocajp/webinar07/Main.java
@@ -9,6 +9,23 @@ public class Main {
         boolean test = true;
         // FALSE не является валидным ключевым словом для boolean-литерала
         //boolean test2 = FALSE; // compile error
+
+
+        /*-
+            Pool of numbers is a range -128...127. Pool work for the next wrapper objects:
+             - Byte
+             - Short
+             - Integer
+             - Long
+        */
+        Long var1 = Long.valueOf(123);
+        Long var2 = Long.valueOf("123");
+        System.out.println(var1 == var2); // true, because var1 and var2 refer to the same cache object (pool of numbers).
+
+        Long var3 = Long.valueOf(223);
+        Long var4 = Long.valueOf("223");
+        System.out.println(var3 == var4); // false, because var3 and var3 are out of range of pool of numbers.
+
     }
 
 }

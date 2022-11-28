19d42ab64276051806bc907e154f4866f80992c7
Alexei <a_subscriber@mail.ru>
2022-11-28 19:03:08 +0200

    GC example

--- src/main/java/ru/otus/ocajp/webinar06/Main.java
+++ src/main/java/ru/otus/ocajp/webinar06/Main.java
@@ -4,5 +4,19 @@ package ru.otus.ocajp.webinar06;
 public class Main {
     public static void main(String args[]) {
         System.out.println("Webinar#06.\nJDK: " + System.getProperty("java.version"));
+
+        /*-
+        The empty String on line 10 is instantiated, and then line 12 appends an "a".
+        However, because the String object is immutable, a new String object is assigned to alpha and the "" object
+        becomes eligible for garbage collection. The next time through the loop, alpha is
+        assigned a new String object, "ab", and the "a" object becomes eligible for garbage
+        collection. The next iteration assigns alpha to "abc" and the "ab" object becomes eligible
+        for garbage collection, and so on.
+        */
+        String alpha = "";
+        for (char current = 'a'; current <= 'z'; current++) {
+            alpha += current;
+        }
+        System.out.println(alpha);
     }
 }

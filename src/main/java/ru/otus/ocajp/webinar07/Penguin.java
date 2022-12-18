2140c4cc48637603ddc594c10b1fcfbcfd098a72
Alexei <a_subscriber@mail.ru>
2022-12-18 12:19:47 +0200

    NPE when iterate list with null element

--- src/main/java/ru/otus/ocajp/webinar07/Penguin.java
+++ src/main/java/ru/otus/ocajp/webinar07/Penguin.java
@@ -3,10 +3,21 @@ package ru.otus.ocajp.webinar07;
 public class Penguin {
     private double test;
 
+    public Penguin() {
+        //howMany(true, {true, true}); // Array initializer is not allowed here
+        int result  = howMany(true, new boolean[2]); // return 2
+        System.out.println("result = "+ result);
+    }
+
+    public int howMany(boolean b, boolean... b2) {
+        return b2.length;
+    }
+
     public static void setTest(Penguin penguin) {
         // Uses dot notation to access the instance variable.
         penguin.test = 11.00;
 
         System.out.println("penguin.test = " + penguin.test);
+
     }
 }

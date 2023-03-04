51d10c20aa78401db11fb8a38abbfe2428f57695
Alexei <a_subscriber@mail.ru>
2023-03-04 19:11:58 +0200

    Virtual methods

--- src/main/java/ru/otus/ocajp/webinar27/VirtualMethodChild.java
+++ src/main/java/ru/otus/ocajp/webinar27/VirtualMethodChild.java
@@ -13,11 +13,16 @@ public class VirtualMethodChild extends VirtualMethodParent {
     }
 
     /*-
-        In other words, even though the parent class VirtualMethodParent defines its own version of getName()
+        Method getName() is overridden in the child class VirtualMethodChild. More importantly, though, the value of
+        the getName() method at runtime in the displayInformation() method is replaced with
+        the value of the implementation in the subclass VirtualMethodChild.
+            In other words, even though the parent class VirtualMethodParent defines its own version of getName()
         and doesn't know anything about the VirtualMethodChild class during compile-time, at runtime the
         instance uses the overridden version of the method, as defined on the instance of the object.
         We emphasize this point by using a reference to the VirtualMethodParent class in the main() method,
         although the result would have been the same if a reference to VirtualMethodChild was used.
+        In compile time Java look all left on sign "=".
+        In runtime Java look all right on sign "=".
      */
     public static void main(String[] args) {
         VirtualMethodParent virtualMethodParent = new VirtualMethodChild();

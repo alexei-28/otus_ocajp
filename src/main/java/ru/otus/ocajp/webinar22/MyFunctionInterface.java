53c12c34c1d2cf62078635a09ebbe79bd6fca793
Alexei <a_subscriber@mail.ru>
2023-03-25 10:38:20 +0200

    Functional interface

--- src/main/java/ru/otus/ocajp/webinar22/MyFunctionInterface.java
+++ src/main/java/ru/otus/ocajp/webinar22/MyFunctionInterface.java
@@ -1,6 +1,6 @@
 package ru.otus.ocajp.webinar22;
 
-// This function interface has 2 abstract methods.
+// This function interface has 2 abstract methods. Only one of them is native abstract method.
 // It's work because second abstract method "equals()" is override from Object class.
 @FunctionalInterface
 public interface MyFunctionInterface {
@@ -13,9 +13,9 @@ public interface MyFunctionInterface {
         System.out.println("static testMethod");
     }
 
-    public boolean myMethod();
+    abstract void myNativeAbstractMethod();
 
     @Override
-    public boolean equals(Object obj);
+    public boolean equals(Object obj); // not native abstract method
 
 }

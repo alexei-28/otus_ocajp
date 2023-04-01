058baee0c5b265ab514d18eea42980333ef0c7bd
Alexei <a_subscriber@mail.ru>
2023-04-01 12:24:41 +0300

    private method is hidden (not override)

--- src/main/java/ru/otus/ocajp/webinar27/BirdParent.java
+++ src/main/java/ru/otus/ocajp/webinar27/BirdParent.java
@@ -1,17 +1,17 @@
 package ru.otus.ocajp.webinar27;
 
 public class BirdParent {
+    /*-
+       Method fly() is marked as private in the parent class PelicanChild, which means it may only be hidden, not overridden.
+       With hidden methods, the specific method used depends on where it is referenced.
+       Since it is referenced within the PelicanChild class, the method BirdParent.fly() was used.
+    */
     private void fly() {
 
         System.out.println("BirdParent is flying");
 
     }
 
-    /*-
-        Method fly() is marked as private in the parent class PelicanChild, which means it may only be hidden, not overridden.
-        With hidden methods, the specific method used depends on where it is referenced.
-        Since it is referenced within the PelicanChild class, the method BirdParent.fly() was used.
-     */
     public static void main(String[] args) {
         BirdParent bird = new PelicanChild();
         bird.fly(); // BirdParent is flying

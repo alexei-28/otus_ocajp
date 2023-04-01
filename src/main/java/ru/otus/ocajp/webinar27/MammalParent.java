5b776381aeeffb02954c126401c57f0ec4e8b6ae
Alexei <a_subscriber@mail.ru>
2023-04-01 11:50:07 +0300

    must exist default constructor in parent class

--- src/main/java/ru/otus/ocajp/webinar27/MammalParent.java
+++ src/main/java/ru/otus/ocajp/webinar27/MammalParent.java
@@ -2,8 +2,6 @@ package ru.otus.ocajp.webinar27;
 
 public class MammalParent {
     public MammalParent(int age) {
-
         System.out.print("Mammal");
-
     }
 }
--- src/main/java/ru/otus/ocajp/webinar27/PlatypusChild.java
+++ src/main/java/ru/otus/ocajp/webinar27/PlatypusChild.java
@@ -1,8 +1,12 @@
 package ru.otus.ocajp.webinar27;
 
+/*-
+  The code will not compile because the parent class MammalParent doesn't define a no-argument constructor,
+  so the first line of a PlatypusChild constructor should be an explicit call to super(int age).
+ */
 public class PlatypusChild extends MammalParent {
     public PlatypusChild() {
-        super(1);// without super(1) will be compile error: constructor MammalParent in class MammalParent cannot be applied to given types
+        super(1);
         System.out.print("Platypus");
     }
 

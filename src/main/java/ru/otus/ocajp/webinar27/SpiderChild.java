909b2a3000846a9582355be38ecc7c1b1c4bc495
Alexei <a_subscriber@mail.ru>
2023-04-01 12:05:54 +0300

    overload method in child class

--- src/main/java/ru/otus/ocajp/webinar27/SpiderChild.java
+++ src/main/java/ru/otus/ocajp/webinar27/SpiderChild.java
@@ -1,13 +1,16 @@
 package ru.otus.ocajp.webinar27;
 
 public class SpiderChild extends ArthropodParent {
-    public void printName(int input) { // override method
+
+    // The printName() method is an overload in SpiderChild, not an override, so both methods may be called
+    public void printName(int input) {
         System.out.print("Spider");
     }
 
     public static void main(String[] args) {
         SpiderChild spider = new SpiderChild();
         spider.printName(4);
-        spider.printName(9.0); // override method
+
+        spider.printName(9.0);
     }
 }

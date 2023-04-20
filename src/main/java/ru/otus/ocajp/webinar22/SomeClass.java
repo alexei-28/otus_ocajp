b7a411b618f296f383e1156e9548d604e3748f19
Alexei <a_subscriber@mail.ru>
2023-04-20 19:22:39 +0300

    Methods equals() and hashCode() are implements in the class Object.

--- src/main/java/ru/otus/ocajp/webinar22/SomeClass.java
+++ src/main/java/ru/otus/ocajp/webinar22/SomeClass.java
@@ -1,10 +1,12 @@
 package ru.otus.ocajp.webinar22;
 
-public class SomeClass implements SomeInterface{
+public class SomeClass implements SomeInterface {
 
     @Override
     public void someMethod() {
         System.out.println("some method");
     }
+
+    // Methods equals() and hashCode() are implements in the class Object.
 
 }

18b705a5467f19a10b60e6896b946651fc9b6703
Alexei <a_subscriber@mail.ru>
2023-04-20 19:13:42 +0300

    interface can override public methods from class Object

---
+++ src/main/java/ru/otus/ocajp/webinar22/SomeInterface.java
@@ -0,0 +1,12 @@
+package ru.otus.ocajp.webinar22;
+
+public interface SomeInterface {
+    public void someMethod();
+
+    @Override
+    public boolean equals(Object obj); // from class Object
+
+    @Override
+    public int hashCode(); // from class Object
+
+}
